beb1f566e3765bd26a779fc5d4626d9dc7f080a4
Eshi44 <e.hameliervolino@gmail.com>
2020-04-09 18:26:08 -0400

    created a schema and seeds sql file. Created a burgers db and a tables burger. added data to seeds file to insert into table

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Vegan burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chili burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', true);
